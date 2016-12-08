ccf7438954680adc6abcb8e41615a65e4296fb9e
hal2001so <hal2001so@outlook.com>
2016-12-08 08:55:33 -0500

    updated HelloWorld_spec.rb to correct typo with HelloWorld value

--- HelloWorld_spec.rb
+++ HelloWorld_spec.rb
@@ -1,9 +1,9 @@
-require './HellowWorld'
+require './HelloWorld'
 
-describe HellowWorld do
+describe HelloWorld do
   context "When testing the HelloWorld class" do
     it "The say_hello method should return 'Hello World'" do
-      hw = HellowWorld.new
+      hw = HelloWorld.new
       message = hw.say_hello
       puts 'Succeed' if expect (message).to eq "Hello, world"
     end
